09323a5e1f1215d82543321140c367a2139f15f1
Al3ncar <alencar29htcss@gmail.com>
2022-01-12 22:41:05 -0300

    the first challenge

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let name = "Igor Alencar"
+
+let number = 20
+
+let date = 2022 - 20
+console.log(date)
+
+
+let text = "Estou mexendo no console.log"
+console.log(text)
+
+let quartaFeira = true
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
